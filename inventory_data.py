35e61f0b548346cbae95114526123364496fa3df
Louis Gitonga <louisgitonga19@gmail.com>
2026-07-08 08:32:28 +0300

    Initial project structure

---
+++ inventory_data.py
@@ -0,0 +1,12 @@
+inventory = [
+    {
+        "id": 1,
+        "name": "Organic Almond Milk",
+        "brand": "Silk",
+        "price": 350,
+        "stock": 20,
+        "barcode": "123456789",
+        "ingredients": "Filtered water, almonds, cane sugar",
+        "source": "mock"
+    }
+]
